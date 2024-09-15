60d9c1cd70f2ed6898a70170374901eed3021747
Shanto <shantothomas61@gmail.com>
2024-09-15 20:59:45 +0530

    first commit

---
+++ DA.py
@@ -0,0 +1,29 @@
+# print ("SHANTO THOMAS: ")
+# print('''HELLO SHANTO
+#        WHATS GOING ON''')
+
+# name = input("Enter Your Namae: ")
+# print(name)
+# age = int(input("Enter Your Age"))
+# print(age)
+# exp1 = eval(input("enter exp"))
+# print(exp1)
+
+# TYPECASTING
+A = "Shanto"
+
+print(type(A))
+B = 23
+print(type(B))
+
+
+
+a = 123
+b= 1.23
+
+print(type(a))
+print(type(b))
+ 
+c = a+b
+print (c) 
+print (type(c))
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+print ("SHANTO THOMAS: ")
+# print('''HELLO SHANTO
+#        WHATS GOING ON''')
+
+# name = input("Enter Your Namae: ")
+# print(name)
+# age = int(input("Enter Your Age"))
+# print(age)
+# exp1 = eval(input("enter exp"))
+# print(exp1)
