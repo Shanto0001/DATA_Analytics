0c5a8f9d9ed9aee0187f301450fd73351e908074
Shanto <shantothomas61@gmail.com>
2024-09-16 11:27:51 +0530

    5th detaIls

--- PROBLEMS.py
+++ PROBLEMS.py
@@ -27,3 +27,23 @@ SWIP_B = 123.3
 SWIP_A = print(SWIP_B)
 
 SWIP_B = print(SWIP_A)
+
+# 4th PROBLEM
+
+swip_1 = 12.2
+swip_1 = int(swip_1)
+print(swip_1)
+
+# 5th Take Details
+print("SHANTO ID CARD")
+ID_name = "Shanto Thomas"
+ID_class = "21BCS07_A"
+ID_adderss = "SATNA_MP"
+ID_branch = "BE_CSE"
+
+print("NAME :- " + ID_name)
+print("CLASS:- " + ID_class)
+print("ADRESSS:- " + ID_adderss)
+print("BRANCH:- " + ID_branch)
+
+6th 
