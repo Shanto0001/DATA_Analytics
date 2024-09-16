e8ece7b6e473dff8a148b25ce97a57cd977e6392
Shanto <shantothomas61@gmail.com>
2024-09-16 11:29:34 +0530

    5th problrm

--- PROBLEMS.py
+++ PROBLEMS.py
@@ -1,26 +1,25 @@
-# # 1ST PROBLEM
-# NAME =  input("Enter Your Name: ")
-# Age =  input("Enter Your Age: ")
-# Address =  input("Enter Your Address: ")
-# print("NAME IS = "+NAME)
-# print("AGE IS = "+ Age)
-# print("ADDRESS IS = "+Address)
+ # 1ST PROBLEM
+NAME =  input("Enter Your Name: ")
+Age =  input("Enter Your Age: ")
+Address =  input("Enter Your Address: ")
+print("NAME IS = "+NAME)
+print("AGE IS = "+ Age)
+print("ADDRESS IS = "+Address)
 
-# # 2ND PROBLEM
-# a = 123
-# a= float(a)
+ # 2ND PROBLEM
+a = 123
+a= float(a)
 
-# b = 12.3
-# b=int(b)
+b = 12.3
+b=int(b)
 
-# print(a)
-# print(type(a))
+print(a)
+print(type(a))
 
-# print(b)
-# print(type(b))
+print(b)
+print(type(b))
 
 #3rd PROBLEM
-
 SWIP_A = "SHANTO"
 SWIP_B = 123.3
 
@@ -28,11 +27,13 @@ SWIP_A = print(SWIP_B)
 
 SWIP_B = print(SWIP_A)
 
-# 4th PROBLEM
 
+# 4th PROBLEM
 swip_1 = 12.2
 swip_1 = int(swip_1)
 print(swip_1)
+
+
 
 # 5th Take Details
 print("SHANTO ID CARD")
@@ -46,4 +47,3 @@ print("CLASS:- " + ID_class)
 print("ADRESSS:- " + ID_adderss)
 print("BRANCH:- " + ID_branch)
 
-6th 
