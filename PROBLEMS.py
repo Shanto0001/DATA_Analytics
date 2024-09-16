33bdb064aade3982d9153d13037fee02e68caaab
Shanto <shantothomas61@gmail.com>
2024-09-16 11:17:47 +0530

    3rdproblem

---
+++ PROBLEMS.py
@@ -0,0 +1,29 @@
+# # 1ST PROBLEM
+# NAME =  input("Enter Your Name: ")
+# Age =  input("Enter Your Age: ")
+# Address =  input("Enter Your Address: ")
+# print("NAME IS = "+NAME)
+# print("AGE IS = "+ Age)
+# print("ADDRESS IS = "+Address)
+
+# # 2ND PROBLEM
+# a = 123
+# a= float(a)
+
+# b = 12.3
+# b=int(b)
+
+# print(a)
+# print(type(a))
+
+# print(b)
+# print(type(b))
+
+#3rd PROBLEM
+
+SWIP_A = "SHANTO"
+SWIP_B = 123.3
+
+SWIP_A = print(SWIP_B)
+
+SWIP_B = print(SWIP_A)
